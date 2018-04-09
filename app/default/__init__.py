57c31cb4e1098d58fcb91440670592cb73e6ae12
ricardo <ricardo.strifeff7@gmail.com>
2018-04-09 16:51:11 -0500

    quitando codigo no util

--- app/default/__init__.py
+++ app/default/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 from config import Config
 
 default = Blueprint('cast', __name__, static_folder=Config.STATIC_FOLDER, static_url_path='')
-from . import routes
+#from . import routes
