3dd093b37b236d38f12ec8b3899e735f3170a7a0
ricardo <ricardo.strifeff7@gmail.com>
2018-07-20 13:17:53 -0500

    more info at uploading data

--- app/common/database.py
+++ app/common/database.py
@@ -5,8 +5,8 @@ __author__ = 'jslvtr'
 
 
 class Database:
-    # URI = "mongodb://richogtz:cloudstrifeFF7!@127.0.0.1:27017"
-    URI = os.environ.get('MONGODB_URI') or "mongodb://127.0.0.1:27017"
+    URI = "mongodb://richogtz:cloudstrifeFF7!@127.0.0.1:27017"
+    # URI = os.environ.get('MONGODB_URI') or "mongodb://127.0.0.1:27017"
     DATABASE = None
 
     @staticmethod
