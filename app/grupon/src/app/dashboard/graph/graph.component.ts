cd6ea82254660dac0120159eefb7000e2891782e
Josep Romagosa <josep_roo@live.com>
2018-07-25 19:04:51 -0500

    dashboard complete

---
+++ app/grupon/src/app/dashboard/graph/graph.component.ts
@@ -0,0 +1,143 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges
+} from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
+})
+export class GraphComponent implements OnInit, OnChanges {
+  @Input() selectedData: any;
+  @Input() lineChartLabels: any;
+  lineChartData = [];
+  lineChartOptions: any = {
+    responsive: true,
+    bezierCurve: false,
+    tooltips: {
+      enabled: true,
+      mode: 'single',
+      callbacks: {
+        label: function (tooltipItems, data) {
+          return '$' + tooltipItems.yLabel;
+        }
+      }
+    },
+    spanGaps: true,
+    pan: {
+      enabled: true,
+      mode: 'x',
+      rangeMin: {
+        x: null,
+        y: 1
+      },
+      rangeMax: {
+        x: null,
+        y: 50000
+      }
+    },
+    zoom: {
+      enabled: true,
+      mode: 'x',
+      rangeMin: {
+        x: null,
+        y: 0
+      },
+      rangeMax: {
+        x: null,
+        y: 50000
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          type: 'time',
+          time: {
+            displayFormats: {
+              day: 'MMM DD',
+              week: 'MMM DD',
+              month: 'MMM DD',
+              quarter: 'MMM DD',
+              year: 'MMM DD'
+            }
+          }
+        }
+      ],
+      yAxes: [{
+        ticks: {
+          callback: function (value) {
+            value += '';
+            const x = value.split('.');
+            let x1 = x[0];
+            const x2 = x.length > 1 ? '.' + x[1] : '';
+            const rgx = /(\d+)(\d{3})/;
+            while (rgx.test(x1)) {
+              x1 = x1.replace(rgx, '$1' + ',' + '$2');
+            }
+            return '$' + x1 + x2;
+          }
+        }
+      }]
+    }
+  };
+  lineChartLegend: Boolean = true;
+  lineChartType: String = 'line';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnChanges() {
+    this.lineChartData = [];
+    Promise.resolve(true).then(() => this.refresh());
+  }
+
+  refresh() {
+    const lines = [];
+    this.selectedData.forEach(product => {
+      const data = {
+        data: [],
+        fill: false,
+        label: product.channel + ' - ' + product.name
+      };
+      this.lineChartLabels.forEach(day => {
+        const price = product.values.find(value => {
+          let day1 = value._id.toUTCString();
+          day1 = day1
+            .split(' ')
+            .slice(0, 4)
+            .join(' ');
+          let day2 = day.toUTCString();
+          day2 = day2
+            .split(' ')
+            .slice(0, 4)
+            .join(' ');
+          if (day1 === day2) {
+            return value;
+          } else {
+            return null;
+          }
+        });
+        if (price) {
+          data.data.push(price.average);
+        } else {
+          data.data.push(null);
+        }
+      });
+      lines.push(data);
+    });
+    this.lineChartData = lines;
+  }
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+}
--- app/grupon/src/app/dashboard/products-list/products-list.component.ts
+++ app/grupon/src/app/dashboard/products-list/products-list.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnChanges {
   @Input() selectedData: any;
+  @Output() clickedProduct: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges() {
   }
-
 }
