110aaf795d978cd336071e997cd685d96cd24499
RichoGtz <ricardo.gutierrez@gavelytics.com>
2019-01-21 21:16:25 -0600

    qwe

--- app/resources/routines.py
+++ app/resources/routines.py
@@ -19,19 +19,16 @@ class Routine(Resource):
             return "Channel not available: " + routine
         logging.info("********************* Start *********************")
         logging.info(f"Starting {routine}")
-        while retries < MAX_RETRIES:
-            try:
-                res = channel.build_tree()
-                logging.info(f"Productos Insertados: {res['inserted']}")
-                logging.info(f"Productos Actualizados: {res['updated']}")
-                break
-            except:
-                logging.exception("Exception occurred")
-                retries += 1
-                if retries == MAX_RETRIES:
-                    logging.exception(f"Skipping {routine}")
+        try:
+            res = channel.build_tree()
+            logging.info(f"Productos Insertados: {res['inserted']}")
+            logging.info(f"Productos Actualizados: {res['updated']}")
+        except:
+            logging.exception("Exception occurred")
+            retries += 1
+            if retries == MAX_RETRIES:
+                logging.exception(f"Skipping {routine}")
 
-            retries = 0
             logging.info(f"Ending {routine}")
         logging.info(f"*********************   End   *********************")
         return "Success!! uploaded: " + routine
